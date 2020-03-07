54f346e4c57d1e037f8740d4f8e24627226a5f3f
Paulo <al.pdias24@gmail.com>
2020-03-06 21:42:44 -0300

    add menus

--- src/bot-telegram-python.py
+++ src/bot-telegram-python.py
@@ -7,7 +7,7 @@ Autor: Paulo https://github.com/alpdias
 # biblioteca para a API do Telegram 'telepot'  https://github.com/nickoala/telepot
 import telepot
 
-# bibliotecas utilizadas para fazer o webscraping, repositorio de como usar --> https://github.com/alpdias/raspagem-web-python
+# bibliotecas utilizadas para fazer o webscraping https://github.com/alpdias/raspagem-web-python
 import bs4
 import requests
 from bs4 import BeautifulSoup
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 # token de acesso
 token = 'token'
 
-# Telegram BOT usando o 'telepot'
+# Telegram BOT
 bot = telepot.Bot(token)
 
 # funçao para enviar as mensagens atravez do BOT
@@ -32,60 +32,51 @@ def receberMensagens(texto):
 
 # funçao para buscar a ultima mensagem recebida pelo BOT e executar os comandos
 def comandos(msg):
+
+    # msg com o menu de controles do BOT dentro do Telegram
+    menu = ('Você pode me controlar enviando esses comandos: \
+\n \
+\n /cotacao - Consultar o valor de ações \
+\n /indice - Consultar o valor de índices \
+\n /dados - Info sobre a fonte de dados \
+\n /menu - Menu de comandos \
+\n /info - Info sobre o BOT \
+\n /ajuda - Obter ajuda')
+
     if msg['text'] == '/start': 
         msg = bot.getUpdates() # recebe a msg/info do BOT em um formato de arquivo JSON
         nome = msg[-1]['message']['from']['first_name'] # recebe o nome da pessoa que enviou a msg
         # msg de aprensetaçao
         apresentacao = f'Olá {nome}! Sejá bem vindo(a), eu sou o @TraderMarketStockBot, um BOT em Python que usa a interface do Telegram \
 para te enviar informações sobre o mercado de ações, de forma rápida e prática.'
-        # msg com o menu de controles do BOT dentro do Telegram
-        menu = ('Você pode me controlar enviando esses comandos: \
-\n \
-\n /cotacao - Consultar o valor de ações \
-\n /indice - Consultar o valor de índices \
-\n /dados - Info sobre a fonte de dados \
-\n /menu - Menu de comandos \
-\n /info - Info sobre o BOT \
-\n /ajuda - Obter ajuda')
         receberMensagens(apresentacao + '\n' + '\n' + menu)
+
     elif msg['text'] == '/cotacao':
         acao = 'Qual o código da ação que você quer consultar?'
         receberMensagens(acao)
+
     elif msg['text'] == '/indice':
         indice = 'Qual o código do índice que você quer consultar?'
         receberMensagens(indice)
+
     elif msg['text'] == '/dados':
         pass
+
     elif msg['text'] ==  '/menu':
-        # msg com o menu de controles do BOT dentro do Telegram
-        menu = ('Você pode me controlar enviando esses comandos: \
-\n \
-\n /cotacao - Consultar o valor de ações \
-\n /indice - Consultar o valor de índices \
-\n /dados - Info sobre a fonte de dados \
-\n /menu - Menu de comandos \
-\n /info - Info sobre o BOT \
-\n /ajuda - Obter ajuda')
         receberMensagens(menu)
+
     elif msg['text'] == '/info':  
         pass
+
     elif msg['text'] == '/ajuda':
         pass
+
     else: 
         msg = bot.getUpdates() # recebe a msg/info do BOT em um formato de arquivo JSON
         nome = msg[-1]['message']['from']['first_name'] # recebe o nome da pessoa que enviou a msg
         # msg para textos ou comandos nao compreendidos/invalidos
         invalido = f'{nome}, desculpe mas não entendi seu comando, ainda estou em construção e não consigo compreender muitas coisas, \
 tente usar uma das opções dentro do meu menu de controles.'
-        # msg com o menu de controles do BOT dentro do Telegram
-        menu = ('Você pode me controlar enviando estes comandos: \
-\n \
-\n /cotacao - Consultar o valor de ações \
-\n /indice - Consultar o valor de índices \
-\n /dados - Info sobre a fonte de dados \
-\n /menu - Menu de comandos \
-\n /info - Info sobre o BOT \
-\n /ajuda - Obter ajuda')
         receberMensagens(invalido + '\n' + '\n' + menu)
 
 
