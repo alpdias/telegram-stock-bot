6c68ec039d5c5536cde413a1359da95e6e197695
Paulo <al.pdias24@gmail.com>
2020-03-22 19:02:03 -0300

    add error to index

--- src/bot-telegram-python.py
+++ src/bot-telegram-python.py
@@ -4,7 +4,7 @@ Criado em 02/2020
 Autor: Paulo https://github.com/alpdias
 '''
 
-# biblioteca para a API do Telegram 'telepot'  https://github.com/nickoala/telepot
+# biblioteca para a API do Telegram 'telepot' https://github.com/nickoala/telepot
 import telepot
 
 # bibliotecas utilizadas para fazer o webscraping https://github.com/alpdias/raspagem-web-python
@@ -27,7 +27,21 @@ def enviarMensagens(msgID, texto):
 def receberMensagens(texto):
     msg = bot.getUpdates() # recebe a msg/info do BOT em um formato de arquivo JSON
     msgID = msg[-1]['message']['chat']['id'] # recebe o ID da conversa
-    global nome = msg[-1]['message']['from']['first_name'] # recebe o nome da pessoa que enviou a msg
+    nome = msg[-1]['message']['from']['first_name'] # recebe o nome da pessoa que enviou a msg
+
+    # msg de aprensetaçao
+    apresentacao = f'Olá {nome}! Sejá bem vindo(a), eu sou o @TraderMarketStockBot, um BOT em Python que usa a interface do Telegram \
+para te enviar informações sobre o mercado de ações, de forma rápida e prática.'
+
+    # msg para textos ou comandos nao compreendidos/invalidos
+    invalido = f'{nome}, desculpe mas não entendi seu comando, ainda estou em construção e não consigo compreender muitas coisas, \
+tente usar uma das opções dentro do meu menu de controles.'
+
+    if texto == 'apresentacao':
+        texto = apresentacao
+    elif texto == 'invalido':
+        texto = invalido + '\n' + '\n' + menu
+    
     enviarMensagens(msgID, texto) 
 
 
@@ -44,9 +58,9 @@ def empresas(codigo):
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     nomeEmpresa = soup.find_all('div',{'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text
     valorEmpresa = float(soup.find_all('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text)
-    empresas = f'Empresa: {nomeEmpresa} \
+    empresa = f'Empresa: {nomeEmpresa} \
 \nPreço atual {codigo.upper()}: {tratamento(valorEmpresa)} - Valor em BRL'
-    return empresas
+    return empresa
 
 
 def indices(codigo):
@@ -54,16 +68,13 @@ def indices(codigo):
     soup = bs4.BeautifulSoup(r.content, 'html.parser')
     nomeIndice = soup.find_all('div',{'class': 'D(ib) Mt(-5px) Mend(20px) Maw(56%)--tab768 Maw(52%) Ov(h) smartphone_Maw(85%) smartphone_Mend(0px)'})[0].find('h1').text.split()
     valorIndice = soup.find_all('div',{'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
-    indices = f'Índice: {nomeIndice[2]} \
+    indice = f'Índice: {nomeIndice[2]} \
 \nValor atual {codigo.upper()}: {valorIndice}'
-    return indices
+    return indice
+
 
 # lista com o menu de controles do BOT dentro do Telegram
 listaComandos = ['/cotacao', '/dados', '/menu', 'info', '/ajuda']
-
-# msg de aprensetaçao
-apresentacao = f'Olá {nome}! Sejá bem vindo(a), eu sou o @TraderMarketStockBot, um BOT em Python que usa a interface do Telegram \
-para te enviar informações sobre o mercado de ações, de forma rápida e prática.'
 
 # msg com o menu de controles do BOT dentro do Telegram
 menu = ('Você pode me controlar enviando esses comandos: \
@@ -78,31 +89,27 @@ menu = ('Você pode me controlar enviando esses comandos: \
 acao = 'Qual o código da ação/índice que você quer consultar?'
 
 # msg com info sobre a fonte de dados utilizada no WebScraping
-fonte = 'Fonte de dados \
+fonte = 'Fonte de dados: \
 \n \
 \n \
 https://finance.yahoo.com/'
 
 # msg com info sobre o BOT e seu funcionamento
-info = ''
+info = 'Para saber mais sobre o meu funcionamento e criação, visite: \
+\n \
+\n \
+https://github.com/alpdias/bot-telegram-python'
 
 # msg com info de ajuda para o usuario
-ajuda = ''
-
-# msg para textos ou comandos nao compreendidos/invalidos
-invalido = f'{nome}, desculpe mas não entendi seu comando, ainda estou em construção e não consigo compreender muitas coisas, \
-tente usar uma das opções dentro do meu menu de controles.'
-
-# msg para caso nada de certo
-inesperado = 'ERRO INESPERADO! \
+ajuda = 'Se precisar de ajuda ou quiser relatar algum problema, entre em contato: \
 \n \
 \n \
-Desculpe, ocorreu um erro inesperado dentro do meu codígo, caso queria relatar e me ajudar selecione o comando \ajuda e envie o problema.'
+https://t.me/alpdias'
 
 # funçao para buscar a ultima mensagem recebida pelo BOT e executar os comandos
 def comandos(msg):
     if msg['text'] == '/start':
-        receberMensagens(apresentacao + '\n' + '\n' + menu)
+        receberMensagens('apresentacao')
     elif msg['text'] == '/cotacao':
         receberMensagens(acao)
     elif msg['text'] == '/dados':
@@ -116,13 +123,13 @@ def comandos(msg):
     elif msg['text'] not in listaComandos:
         try:
             print(empresas(msg['text']))
-        except:
+        except IndexError:
             try:
                 print(indices(msg['text']))
-            except:
-                receberMensagens(invalido + '\n' + '\n' + menu)
+            except IndexError:
+                receberMensagens('Desculpe, mas não encontrei essa ação/índice com esse código, tente novamente!')
     else:
-        receberMensagens(inesperado)
+        receberMensagens('invalido')
 
 
 # loop para procurar novas msgs e executar as funçoes
